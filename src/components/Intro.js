aaa90797277ffcb46f96b89734ebebaf3c2b0c31
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-03-06 21:49:27 +0100

    Intro improvements

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -16,11 +16,11 @@ const Intro = () =>
     <p>
       You, Dina Pardijs, have realised that the best way to help the world is
       not to focus on the needy, but to become a psychic and learn what's going
-      to happen ahead of time so that you can stop it. As a member of the
-      Psychic Institute of Supernatural Studies you receive access to their
+      to happen ahead of time so that you can stop it. Upon joining the
+      Psychic Institute of Supernatural Studies you received access to their
       rigorous online training program. In the first course you took, Future
       Learning and Psychic Senitivity, you beat the previous record and quickly
-      drew the attention of the disciplines heavy-hitters. They decided to up
+      drew the attention of the discipline's heavy-hitters. They decided to up
       the stakes and put you through Cranial Overloads and Cognitive Kicks -
       the Institute's most challenging course. Your ability to use telekenis
       to rip paper both figuratively and literally tore up the course record 
@@ -44,10 +44,13 @@ const Intro = () =>
       <p>No doubt you're aware of Chore Wars. I have good reason to believe that
       one of the straight white men in our organisation will become so frustrated
       with his partner for proving that she does more cleaning than him, that
-      he'll murder her in cold blood. It's vital that we find out who this man
-      is and how he'll commit the murder. I'm hoping that someone with such high 
-      PISSFLaPS and PISSCOCK scores will be able to convince the police to
-      arrest him before he ruins our organisation's reputation.</p>
+      he'll murder her in cold blood. Most of us straight white men are thoroughly
+      good chaps and we can't have one man ruin it for the rest of us.</p>
+
+      <p>It's vital that we find out who this man is and how he'll commit the 
+      murder. I'm hoping that someone with such high PISSFLaPS and PISSCOCK scores 
+      will be able to convince the police to arrest him before he ruins our 
+      organisation's reputation.</p>
 
       <p>I've provided you with a list of names of suspects. Be warned, however,
       not all of them are such gifted psychics as you. Some of them show almost
